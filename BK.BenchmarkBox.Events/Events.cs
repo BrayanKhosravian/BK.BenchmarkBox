4c1b5e5b0ea52297b567635b77ba0e081529e82c
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2020-04-26 22:30:51 +0200

    - verified that small structs are faster as event args

---
+++ BK.BenchmarkBox.Events/Events.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace BK.BenchmarkBox.Events
+{
+	public class ClassEventArg : EventArgs
+	{
+		public int Number { get; set; }
+	}
+
+	public struct StructEventArgs
+	{
+		public int Number { get; set; }
+	}
+
+	public class Events
+	{
+		public EventHandler<ClassEventArg> ClassEventHandler;
+		public EventHandler<StructEventArgs> StructEventHandler;
+		private readonly int[] _collection;
+		public Events()
+		{
+			_collection = Enumerable.Range(Int32.MaxValue - 1001, 1000).ToArray();
+			ClassEventHandler += OnClassEventHandler;
+			StructEventHandler += OnStructEventHandler;
+		}
+
+		[Benchmark]
+		public void ClassEvent() => Array.ForEach<int>(_collection, item => ClassEventHandler.Invoke(null, new ClassEventArg(){Number = item}));
+
+		[Benchmark]
+		public void StructEvent() =>  Array.ForEach<int>(_collection, item => StructEventHandler.Invoke(null, new StructEventArgs(){Number = item}));
+
+		private static void OnClassEventHandler(object? sender, ClassEventArg e) { }
+
+		private static void OnStructEventHandler(object? sender, StructEventArgs e) { }
+
+	}
+}
---
+++ BK.BenchmarkBox.Events/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using static System.Int32;
+
+namespace BK.BenchmarkBox.Events
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			var summary = BenchmarkRunner.Run<Events>();
+
+		}
+
+	}
+}
